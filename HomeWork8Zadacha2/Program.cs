2a1a6c77cf6bcc49a7da720ee9cb1cf0cf1756d3
AleksandrSavelyev <punk558721973@gmail.com>
2020-09-14 00:19:05 +0300

    soxranenuye

---
+++ HomeWork8Zadacha2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace HomeWork8Zadacha2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle t = new Triangle();
+            for (; ; )
+            {
+                Console.Write("введите сторону AB:\t");
+                t.A = NumVal();
+                Console.Write("введите сторону BC:\t");
+                t.B = NumVal();
+                Console.Write("введите сторону CA:\t");
+                t.C = NumVal();
+                if (t.IsTriangle)
+                    break;
+                else
+                    Console.WriteLine("не существует треугольника с такими длиннами сторон\nвведите стороны заново:");
+            }
+            t.GetInfo();
+            Console.WriteLine(t.GetPerimetr());
+            Console.WriteLine(t.GetArea());
+        }
+        static int NumVal()
+        {
+            for (; ; )
+            {
+                if (int.TryParse(Console.ReadLine(), out int b))
+                    return b;
+                else
+                    Console.Write("неверные данные! повторите ввод:\t");
+            }
+        }
+    }
+}
---
+++ HomeWork8Zadacha2/Triangle.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork8Zadacha2
+{
+    class Triangle
+    {
+        private int a, b, c;
+        public Triangle() { }
+        public Triangle(int a, int b, int c)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+
+        public int A
+        {
+            get { return a; }
+            set { a = value; }
+
+        }
+
+        public int B
+        {
+            get { return b; }
+            set { b = value; }
+        }
+
+        public int C
+        {
+            get { return c; }
+            set { c = value; }
+        }
+        public bool IsTriangle
+        {
+            get
+            {
+                if (a + b > c && a + c > b && b + c > a)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"сторона AB={a} сторона BC={b} сторона CA={c}");
+        }
+        public string GetPerimetr()
+        {
+            int P = a + b + c;
+            return $"периметр треугольника рвен {P}";
+        }
+        public string GetArea()
+        {
+            double x = a + b + c;
+            double Ppolu = x / 2;
+            double S = Math.Sqrt(Ppolu * (Ppolu - a) * (Ppolu - b) * (Ppolu - c));
+            return $"площадь треугольника равна {S}";
+        }
+    }
+}
+
+        
+    
+
